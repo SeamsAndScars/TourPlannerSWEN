1b6f1cf9e7a284777a435332398066ea6243c6fa
SeamsAndScars <102183435+SeamsAndScars@users.noreply.github.com>
2023-06-11 15:58:10 +0200

    Added Db conncetion, entity framework, npgsql

--- TourPlanner_Client/DAL/Database/Connection.cs
+++ TourPlanner_Client/DAL/Database/Connection.cs
@@ -1,10 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Configuration;
+using Npgsql;
 
 namespace TourPlanner_Client.DAL.Database
 {
-    internal class Connection
+    public class Connection
     {
+        public NpgsqlConnection GetConnection()
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["MyDbConnection"].ConnectionString;
+            return new NpgsqlConnection(connectionString);
+        }
     }
+
 }
